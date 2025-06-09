433be90face5008d3d8b2fde9cf02ff45958f9ae
haryu90 <ring4640@gmail.com>
2025-06-09 23:39:34 +0900

    main.py

--- main.py
+++ main.py
@@ -43,9 +43,9 @@ async def 환영(ctx, *members: discord.Member):
 
     mentions = " ".join(member.mention for member in members)
     message = (
-    f"# <a:84931announcement:1381626468735385600>   {mentions} 님 환영합니다!\n\n"
+f"# <a:84931announcement:1381626468735385600>   {mentions} 님 환영합니다!\n\n"
         f"<a:51047animatedarrowwhite:1381626541150175332>   {mentions} 님 𝐌𝐀𝐈𝐃 𝐌𝐨𝐨𝐍에 오신 것을 환영합니다!\n\n"
-        f" <a:51047animatedarrowwhite:1381626541150175332> <#1381099323764375672> 에서 규칙을 꼭 확인해주세요!\n"
+        f" <a:51047animatedarrowwhite:1381626541150175332> <#1381621263730086060>  에서 규칙을 꼭 확인해주세요!\n"
         f"<:6430pinkribbon:1381626681357238452>   규칙을 읽지 않아 생기는 불이익은 책임지지 않아요!\n\n"
         f" <a:51047animatedarrowwhite:1381626541150175332> 적응이 어렵다면 <@&1381621262291570842>   를 맨션해주세요!\n\n"
         f"<:3141coquettebow:1381626675489669220>    앞으로 잘 부탁드려요!\n"
